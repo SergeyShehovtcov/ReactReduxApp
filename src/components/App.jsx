94e723a521c4e9526138a78110a7077e6eca2530
Сергей Шеховцов <sshehovtcov@gmail.com>
2023-10-30 01:30:07 +0500

    lesson 4

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./app.less";
+import {useDispatch, useSelector} from "react-redux";
+import {setCount} from "../reducers/reposReducer";
+
+const App = () => {
+    const dispath = useDispatch();
+    const count = useSelector(state => state.repos.count);
+    function onCountClick() {
+        dispath(setCount(5));
+    };
+    return (
+        <div className="app">
+            <button onClick={() => onCountClick()}>COUNT</button>
+            <div>{count}</div>
+        </div>
+    );
+};
+
+export default App;
