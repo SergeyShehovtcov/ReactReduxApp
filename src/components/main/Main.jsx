c48c51eee37c8057e2af6e1f5544dc0c0cf9230e
Sergey <32293129+SergeyShehovtcov@users.noreply.github.com>
2023-10-31 11:39:08 +0000

    lesson 6

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Repo from "./repo/Repo";
 import "./main.less"
 import {useDispatch, useSelector} from "react-redux";
@@ -7,16 +7,40 @@ import {getRepos} from "../actions/repos";
 const Main = () => {
     const dispatch = useDispatch();
     const repos = useSelector(state => state.repos.items);
+    const isFetching = useSelector(state => state.repos.isFetching);
+    const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
-        dispatch(getRepos())
+        dispatch(getRepos());
     }, []);
+
+    function searchHandler() {
+        dispatch(getRepos(searchValue));
+    }
 
     return(
         <div>
-            {repos.map(repo =>
-                <Repo key={repo.html_url} repo={repo} />
-            )}
+            <div className="search">
+                <input
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    type="text"
+                    placeholder="Введите имя репозитория"
+                    className="search-input"
+                />
+                <button
+                    onClick={() => searchHandler()}
+                    className="search-btn"
+                >
+                    Поиск
+                </button>
+            </div>
+            {isFetching === false
+             ?  repos.map(repo => <Repo key={repo.html_url} repo={repo} />)
+             :  <div className="fetching">
+
+                </div>
+            }
         </div>
     );
 };
